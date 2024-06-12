ac0ee8702c7b2fbd2370991a9dd7d39c706d5b5c
antonioarden <62288418+antonioarden@users.noreply.github.com>
2024-06-12 16:18:41 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ var links = [
   "https://www.figma.com/proto/qzDd6Rhf03IV9RIlMe5SKm/snapjobs?page-id=139%3A499&type=design&node-id=140-499&viewport=582%2C626%2C0.08&t=X6cOiWQRL7RTOTMt-1&scaling=scale-down&starting-point-node-id=140%3A499&show-proto-sidebar=1",
   "https://www.figma.com/proto/jL6hXhuHJPsGWsKZJn1H6A/XXI-Redesign?page-id=0%3A1&type=design&node-id=132-436&viewport=179%2C1801%2C0.25&t=7TNEm3sWXzNN5Ztd-1&scaling=scale-down&starting-point-node-id=120%3A204&show-proto-sidebar=1",
   "https://www.figma.com/proto/3cuJbxPEN9u3ktgq7guMld/JucyJooz?page-id=0%3A1&type=design&node-id=5-188&viewport=819%2C328%2C0.09&t=haHyZLP5hDxI2A9d-1&scaling=contain&starting-point-node-id=5%3A188&show-proto-sidebar=1",
+  "https://antonioarden.github.io/Myresume/",
 ];
 
 function linkProject(index) {
